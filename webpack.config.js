7c9638aeec15abe61234338000709054b2783380
Ben Naylor <nayben@gmail.com>
2019-12-11 10:34:56 +0100

    chore: reorganising config

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: slsw.lib.entries,
   devtool: 'source-map',
   resolve: {
-    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+    extensions: ['.ts'],
   },
   output: {
     libraryTarget: 'commonjs',
@@ -16,8 +16,11 @@ module.exports = {
   target: 'node',
   module: {
     rules: [
-      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-      { test: /\.tsx?$/, loader: 'ts-loader' },
+      {
+        test: /\.ts$/,
+        exclude: /\.test\.ts$/,
+        loader: 'ts-loader'
+      },
     ],
   },
 };
